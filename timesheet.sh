f1207f4ca5323e11f1f49c3d6580c06814aa846f
Karel Schwab <schwabk9@protonmail.com>
2023-04-01 13:38:28 +0200

    Add basic usage in comments

--- timesheet.sh
+++ timesheet.sh
@@ -1,10 +1,10 @@
 # timesheet add <task> - Add a take to the timesheet
 
-# timesheet list - List current week timesheet
-# ? timesheet list <date>
-# ? timesheet list <start_date> <end_date>
+# timesheet list - List current days tasks
+# timesheet list <date> - List tasks for the day of the date
+# timesheet list <start_date> <end_date> - List tasks for the range of dates
 
-#
+# timesheet remove - Removes the last added task
 
 echo "Test"
 
