cb2124ade2364349193fbf93f6f110834613f7d3
Karel Schwab <schwabk9@protonmail.com>
2023-04-01 13:28:45 +0200

    Initial commit

---
+++ timesheet.sh
@@ -0,0 +1,11 @@
+# timesheet add <task> - Add a take to the timesheet
+
+# timesheet list - List current week timesheet
+# ? timesheet list <date>
+# ? timesheet list <start_date> <end_date>
+
+#
+
+echo "Test"
+
+exit 0
